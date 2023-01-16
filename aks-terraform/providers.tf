d74afe8f3c9c2b2708ec9f3a9937df7cadae4564
Yogesh0777 <94049153+Yogesh0777@users.noreply.github.com>
2023-01-16 13:14:16 +0530

    Update providers.tf

--- aks-terraform/providers.tf
+++ aks-terraform/providers.tf
@@ -2,14 +2,30 @@ provider "azurerm" {
   features {}
 }
 
+#terraform {
+ # required_providers {
+  #  azurerm = {
+   #   source  = "hashicorp/azurerm"
+    #  version = "2.39.0"
+    #}
+  #}
+#}
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.39.0"
-    }
-  }
-}
-provider "azurerm" {
+  
+required_version = ">= 0.12"
+backend "azurerm" {
+  storage_account_name = "terraformstorageaccount"
+  container_name = "terraform"
+  key = "terraform.tfstate"
+  access_key = "storagekey"
   features {}
+}
+}
+provider "azurerm"{
+version = "~>2.0"
+features {}
+}
+resource "azurerm_resource_group" "dev" {
+    name = "pullerraform"
+    location = "EastUS"
 }
